62ff53a2d6b47102d2dd8c572038b8409f7a5e90
MarcielSouza <marciel.hsouza@hotmail.com>
2022-06-16 18:05:47 -0300

    Regex para python - 002

---
+++ Regex_Funcoes.py
@@ -0,0 +1,21 @@
+import re
+
+print("\n000-----------------------------------------\n")
+
+# O findall() : A função retorna uma lista contendo todas as correspondências.
+
+# Example: Exemplo: Imprime uma lista de todas as correspondências:
+
+txta = 'The rain Spain'
+
+#Retorna uma lista contendo todas as ocorrências de "ai":
+
+a = re. findall('ai',txta)
+
+print(a)
+
+#----==============================================================================================================================================================----#
+
+print("\n001-----------------------------------------\n")
+
+
